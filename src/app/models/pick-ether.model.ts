6ef1acdcdd56fdde25f699c58f0ec5ef2174f755
Abou MLANAO <abou.mlanao@gmail.com>
2022-08-12 20:32:03 +0200

    _redirects for netlify

---
+++ src/app/models/pick-ether.model.ts
@@ -0,0 +1,12 @@
+export class pickEther{
+    title : any = String;   //peut être déclaré de ces 2 manières
+    description!: string;
+    createdDate!: Date;
+    like!: number;
+    urlImg!: string;
+    
+    constructor(title:string, description: string, createdDate: Date, like:number, urlImg:string){
+
+    }
+  
+}
