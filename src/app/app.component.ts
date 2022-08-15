09ae3b09ef52789655392aa62c999ed11d41d404
Abou MLANAO <abou.mlanao@gmail.com>
2022-08-16 00:09:47 +0200

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,67 @@ import { pickEther } from './models/pick-ether.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  myPick : any = pickEther ;
-  myPick2!: pickEther;
-  myPick3!: pickEther;
+  myPicks : any = pickEther ;  
+  // myPicks : any  ; fonctionne aussi
 
   ngOnInit(): void {
-    this.myPick = new pickEther(
-      'Fraisouw',
-      "Palmier de secours",
-      new Date(),
-      0,
-      "../../assets/img/01-fraisealombre.jpg"
-    )
+    this.myPicks = [{
+      title: 'Fraisouw',
+      description: "Palmier de secours",
+      createdDate!: new Date(),
+      like: 0,
+      urlImg: "../../assets/img/01-fraisealombre.jpg",
+      location: 'Cap D\'Agde'
+    },
 
-    this.myPick2 = new pickEther(
-      'cocktail-cucumber-rose-soda',
-      "La pétillance qui pétille",
-      new Date(),
-      0,
-      "../../assets/img/02-cocktail-cucumber-rose-soda.gif"
-    )
+    // this.myPick = new pickEther(      
+    //   'Fraisouw',
+    //   "Palmier de secours",
+    //   new Date(),
+    //   0,
+    //   "../../assets/img/01-fraisealombre.jpg")
+ {
+      title: 'cocktail-cucumber-rose-soda',
+      description: "La pétillance qui pétille",
+      createdDate!: new Date(),
+      like: 0,
+      urlImg: ".../../assets/img/02-cocktail-cucumber-rose-soda.gif",
+      // localition: 'Los Angeles'
+    },
+      // this.myPick2 = new pickEther(
+      //   'cocktail-cucumber-rose-soda',
+      //   "La pétillance qui pétille",
+      //   new Date(),
+      //   0,
+      //   "../../assets/img/02-cocktail-cucumber-rose-soda.gif"
+      // )
+
+     {
+        title: '03-fraise-water-summer',
+        description: "Rain de fraise",
+        createdDate!: new Date(),
+        like: 0,
+        urlImg: "../../assets/img/03-fraise-water-summer.gif",
+        location: 'Acapulco'
+      }
+
+    ] 
+  // this.myPick3 = new pickEther(
+  //   '03-fraise-water-summer',
+  //   "Rain de fraise",
+  //   new Date(),
+  //   0,
+  //   "../../assets/img/03-fraise-water-summer.gif"
+  // )
 
 
-    this.myPick3 = new pickEther(
-      '03-fraise-water-summer',
-      "Rain de fraise",
-      new Date(),
-      0,
-      "../../assets/img/03-fraise-water-summer.gif"
-    )
+console.log(this.myPicks);
 
 
   }
+
+
+
+
+
 }
--- src/app/models/pick-ether.model.ts
+++ src/app/models/pick-ether.model.ts
@@ -1,9 +1,10 @@
-// export class pickEther{
-//     title : any = String;   //peut être déclaré de ces 2 manières
-//     description: string;
-//     createdDate: Date;
-//     like: number;
-//     urlImg: string;
+export class pickEther{
+    title : any = String;   //peut être déclaré de ces 2 manières
+    description!: string;
+    createdDate!: Date;
+    like!: number;
+    urlImg!: string;
+    location?: string;
     
 //     constructor(title:string, description: string, createdDate: Date, like:number, urlImg:string){
 //         this.title = title;
@@ -15,14 +16,15 @@
   
 // }
 
-export class pickEther{
-                                    //il est possible de ne pas déclarer comme fait dans l'exemple en précisant public devant
-    constructor(public title:string, 
-                public description: string, 
-                public createdDate: Date, 
-                public like:number, 
-                public urlImg:string) {
+// export class pickEther{
+//                                     //il est possible de ne pas déclarer comme fait dans l'exemple en précisant public devant
+//     constructor(public title:string, 
+//                 public description: string, 
+//                 public createdDate: Date, 
+//                 public like: number, 
+//                 public urlImg: string,
+//                 public localition?: string,) {  //? permet de 
         
-    }
+//     }
   
 }
